5d2f1dbceaa9d54253a85c611d9f4694e66b9de8
Rocamr <100881982+Rocamr@users.noreply.github.com>
2022-04-08 10:18:27 -0600

    Add files via upload

---
+++ Practica 2.pl
@@ -0,0 +1,1 @@
+sumlist(lis,s):-
---
+++ Practica1Prob.pl
@@ -0,0 +1,25 @@
+sumlist[], S).
+sumlist([X|Xs],S):-
+    S is Xs,
+    sumlist(Xs,S).
+
+largopar([]).
+largopar([_|Xs]):-
+          largopar(Xs),
+          W is Xs +1,
+          I is W mod 2,
+          (I!=0,largopar(Xs);
+          disply(Xs),disply(','),largopar(Xs)).
+
+largoIpar([]).
+largoIpar([_|Xs]):-
+          largoIpar(Xs),
+          W is Xs +2,
+          I is W mod 2,
+          (I=0,largopar(Xs);
+          disply(Xs),disply(','),largoIpar(Xs)).
+sublist(Xs, Ys):-
+          my_append(_, Bs, Ys),
+          my_append(Xs, _, Bs).
+invierte([],[]) :- my_reverse(A, D).
+split([H|T],E,[H|F],S) :- split(T,E,F,S).
